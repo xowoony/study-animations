c254be4b9a8d435148aeb2a1cddc69352c555ed1
xowoony <trumanblack@hanmail.net>
2023-05-11 16:04:29 +0900

    Variants 연습하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { styled } from "styled-components";
 import { motion } from "framer-motion";
-import { start } from "repl";
 
 // style
 const Wrapper = styled.div`
@@ -11,29 +10,66 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+// 박스
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 `;
 
-// Variants 사용하기
-// 1. 코드가 깔끔해짐 2. 많은 애니메이션들을 하나로 연결시켜줌
-const myVars = {
-  // 초기상태 - start라고 이름 지어 주겠음
-  // 이전의 코드에서 initial 부분을 이쪽으로 옮겨주겠음.
-  start: { scale: 0 },
-  // 최종 상태 - end라고 이름 지어 주겠음
-  // 이전의 코드에서 animate,  transition부분을 이쪽으로 옮겨주겠음.
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+// 원
+const Circle = styled(motion.div)`
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  place-self: center; // 원들이 박스에서 center로 위치하게 됨
+`;
+
+// Variants
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 2, // box가 마무리 될때까지 2초 걸림
+      bounce: 0.5,
+      // delayChildren: 0.5, // 모든 자식 통째로 0.5초의 delay주기
+      staggerChildren: 0.2, // 자식1, 자식2, 자식3, 자식4가 차례로 0.2초 간격으로 나옴
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10, 
+  },
+  end: {
+    opacity: 1,
+    y: 0, // 밑에서부터 위로 올라옴
+  },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial={myVars.start} animate={myVars.end}/>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
