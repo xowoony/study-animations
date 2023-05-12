8b5213250524ade1f533c77a9e0052f30d07e344
xowoony <trumanblack@hanmail.net>
2023-05-12 17:54:31 +0900

    useEffect로 x값 추적하여 콘솔에 찍어보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { styled } from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 // style
 const Wrapper = styled.div`
@@ -9,18 +9,6 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-// 큰 박스
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* overflow: hidden; */
 `;
 
 // 박스
@@ -32,29 +20,19 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-// Variants
-const boxVariants = {
-  hover: { scale: 1.2, rotateZ: 90 },
-  tap: { borderRadius: "100px" },
-};
-
 function App() {
-  // useRef 작성, 기본값 null, 타입스크립트 HTMLDIVELEMENT 적용
-  // 작성 후 BiggerBox 컴포넌트에 전달
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  
+// useEffect 사용으로 x값 추적하기
+// x값이 바뀔때마다 x값이 콘솔에 찍혀나옴.
+  useEffect(() => {
+    x.on("change", () => console.log(x.get()));
+  }, [x]);
+
+
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragElastic={0.5} // 값이 작을수록 무거운 용수철
-          dragSnapToOrigin
-          dragConstraints={biggerBoxRef} // (300 - 중앙박스의 절반)
-          variants={boxVariants}
-          whileHover="hover" // hover시 90도 z축 회전, 1.2배 커짐
-          whileTap="tap" // 클릭했을 경우 scale1로 돌리고 원이 됨
-        ></Box>
-      </BiggerBox>
+      <Box style={{ x }} drag="x" dragSnapToOrigin></Box>
     </Wrapper>
   );
 }
