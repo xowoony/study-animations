1b2ee360eed0a3273e9af76407cc3a2666963bc1
xowoony <trumanblack@hanmail.net>
2023-05-12 14:12:31 +0900

    useRef를 사용하여 constraints 적용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 // style
 const Wrapper = styled.div`
@@ -38,12 +39,15 @@ const boxVariants = {
 };
 
 function App() {
+  // useRef 작성, 기본값 null, 타입스크립트 HTMLDIVELEMENT 적용
+  // 작성 후 BiggerBox 컴포넌트에 전달
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <BiggerBox>
+      <BiggerBox ref={biggerBoxRef}>
         <Box
           drag
-          dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }} // (300 - 중앙박스의 절반)
+          dragConstraints={biggerBoxRef} // (300 - 중앙박스의 절반)
           variants={boxVariants}
           whileHover="hover" // hover시 90도 z축 회전, 1.2배 커짐
           whileTap="tap" // 클릭했을 경우 scale1로 돌리고 원이 됨
