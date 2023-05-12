fb2143ee2429df861f9260b3833b8e1f581a2f3c
xowoony <trumanblack@hanmail.net>
2023-05-12 14:01:47 +0900

    dragConstraints

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+// 큰 박스
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
 // 박스
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -22,20 +34,21 @@ const Box = styled(motion.div)`
 // Variants
 const boxVariants = {
   hover: { scale: 1.2, rotateZ: 90 },
-  tap: { borderRadius: "100px", scale: 1 },
-  drag: { backgroundColor: "rgb(46, 204, 113)" , transition:{duration: 5}},
+  tap: { borderRadius: "100px" },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        drag
-        whileDrag="drag" // drag 하는 도중 - background-color 작성시 문자열로 적어주면 안되고 rgb 로 적어줘야 함.
-        variants={boxVariants}
-        whileHover="hover" // hover시 90도 z축 회전, 1.2배 커짐
-        whileTap="tap" // 클릭했을 경우 scale1로 돌리고 원이 됨
-      ></Box>
+      <BiggerBox>
+        <Box
+          drag
+          dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }} // (300 - 중앙박스의 절반)
+          variants={boxVariants}
+          whileHover="hover" // hover시 90도 z축 회전, 1.2배 커짐
+          whileTap="tap" // 클릭했을 경우 scale1로 돌리고 원이 됨
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
