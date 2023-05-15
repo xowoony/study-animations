21bdd7df9f6abcba8e40993b7b148ae47d40ed00
xowoony <trumanblack@hanmail.net>
2023-05-15 11:07:41 +0900

    특정 property의 애니메이션만 변경하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,56 @@
 import { styled } from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useScroll,
-  useTransform,
-} from "framer-motion";
+import { motion } from "framer-motion";
 
 // style
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-// 박스
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  // styled-component에서 path의 stroke, stroke-width를 줘도 상관없음.
+  path {
+    stroke: rgb(134, 73, 255); // stroke를 작성하면 선이 생김
+    stroke-width: 2; // 선굵기
+  }
 `;
 
+// variants
+const svg = {
+  start: { pathLength: 0, fill: "rgba(134, 73, 255, 0)" },
+  end: {
+    fill: "rgba(134, 73, 255, 1)",
+    pathLength: 1,
+  },
+};
+
 function App() {
-  const x = useMotionValue(0);
-
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(255, 255, 255), rgb(1, 10, 29))",
-      "linear-gradient(135deg, rgb(201, 207, 21), #060c47",
-    ]
-  );
-
-  // useScroll
-  const { scrollYProgress } = useScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box
-        style={{ x, rotateZ: rotateZ, scale: scale }}
-        drag="x"
-        dragSnapToOrigin
-      ></Box>
+    <Wrapper>
+      <Svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <motion.path
+          variants={svg}
+          //처음상태
+          initial="start"
+          // 최종상태
+          animate="end"
+          // property의 transition 시간 정하기
+          transition={{
+            default: { duration: 5 }, // 모든 트랜지션의 default duration을 5로 설정
+            fill: { duration: 2, delay: 4 }, // fill만 duration을 2, delay를 5로 설정
+          }}
+          d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z"
+        />
+      </Svg>
     </Wrapper>
   );
 }
