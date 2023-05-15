22079fbfa8ad7b7cf67d391aca33b9f07a8714d2
xowoony <trumanblack@hanmail.net>
2023-05-15 17:25:44 +0900

    AnimatePresence를 이용하여 슬라이드 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -6,58 +6,74 @@ import { useState } from "react";
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
   align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   background: linear-gradient(135deg, rgb(252, 191, 248), rgb(21, 151, 203));
 `;
 
 const Box = styled(motion.div)`
+  position: absolute;
+  top: 180px;
+
+  width: 21rem;
+  height: 10rem;
   background-color: white;
-  border-radius: 14px;
-  width: 300px;
-  height: 300px;
-  top: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  box-shadow: 0 2px 3px gray, 0 10px 20px gray;
+
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  font-size: 28px;
 `;
 
-const boxVariants = {
-  initial: {
+const BoxVariants = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: { duration: 0.7 },
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 50,
+    transition: { duration: 0.7 },
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
+  const [visible, setVisible] = useState(1);
+  const nextToggle = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
 
-  const toggleShowing = () => {
-    setShowing((prev) => !prev);
-  };
-  // exit : element가 사라질때 animation 발생
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={BoxVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
+        s
       </AnimatePresence>
+      <button onClick={nextToggle}>next</button>
     </Wrapper>
   );
 }
