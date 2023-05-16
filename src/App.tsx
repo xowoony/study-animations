d2714fd066335c5f279548edd7c82aaa0212715a
xowoony <trumanblack@hanmail.net>
2023-05-16 22:43:18 +0900

    클릭시 팝업창 뜨게 하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 // style
@@ -13,40 +13,58 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(252, 191, 248), rgb(21, 151, 203));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 300px;
-  height: 300px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  height: 200px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
-  align-items: center;
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
   display: flex;
-  flex-direction: row;
+  align-items: center;
   justify-content: center;
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  width: 50px;
-  height: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
-`;
-
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  // 어떤 id를 클릭했는지
+  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>
-        {!clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+    <Wrapper>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+      {/* 클릭한 상태면 Overlay를 보여주고 아니라면 null */}
+      {/* exit를 animate해주기 위해 AnimatePresence로 감싸기 */}
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0,0,0, 0)" }}
+            animate={{ backgroundColor: "rgba(0,0,0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0,0,0, 0)" }}
+          >
+            {/* layoutId에 현재 선택된 id를 넣어줌으로 인해 해당 팝업이 뜨게 된다. */}
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
         ) : null}
-      </Box>
-      <Box>
-        {clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 0, scale:3 }} />
-        ) : null}
-      </Box>
+      </AnimatePresence>
     </Wrapper>
   );
 }
