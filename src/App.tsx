d66d6d48dc0087b9bca0690dd41e61335a3a219e
xowoony <trumanblack@hanmail.net>
2023-05-16 17:52:10 +0900

    layout, layoutId 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 // style
@@ -8,85 +8,45 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   align-items: center;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   background: linear-gradient(135deg, rgb(252, 191, 248), rgb(21, 151, 203));
 `;
 
 const Box = styled(motion.div)`
-  position: absolute;
-  top: 180px;
-
-  width: 21rem;
-  height: 10rem;
-  background-color: white;
-  border-radius: 1rem;
-  box-shadow: 0 2px 3px gray, 0 10px 20px gray;
-
+  width: 300px;
+  height: 300px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
   align-items: center;
   display: flex;
   flex-direction: row;
   justify-content: center;
-
-  font-size: 28px;
 `;
 
-const BoxVariants = {
-  // entry를 function으로 만들어준다.
-  // 밑에 Box 컴포넌트 안 custom={back}과 (back: boolean) 이 둘의 이름이 달라도 상관은 없다.
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 0.7 },
-  },
-
-  // exit을 function으로 만들어준다.
-  // 밑에 Box 컴포넌트 안 custom={back}과 (back: boolean) 이 둘의 이름이 달라도 상관은 없다.
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 0.7 },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  width: 50px;
+  height: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const nextToggle = () => {
-    setBack(false); // next버튼을 눌렀을 때에는 setBack을 fasle로
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevToggle = () => {
-    setBack(true); // prev버튼을 눌렀을 때에는 setBack을 true로
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      {/* mode="wait" 을 작성하면 1번박스가 완전히 사라진 후 2번박스가 나타나게 된다.*/}
-      <AnimatePresence  mode="wait" custom={back}>
-        <Box
-          custom={back} // custom에 back을 넣어준다. 여기는 true 또는 false가 될 것이다.
-          variants={BoxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextToggle}>next</button>
-      <button onClick={prevToggle}>prev</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale:3 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
